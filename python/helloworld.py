7f1b92b06223fb72be325b695971744e44f0d48b
Diana Alfaro <111534967+alfalfita@users.noreply.github.com>
2023-06-08 13:44:24 -0500

    feat(python): add script

---
+++ python/helloworld.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Bootcamp AWS Ignite 2023 python'
+
+if __name__ == '__main__':
+    app.run(port=8080)
